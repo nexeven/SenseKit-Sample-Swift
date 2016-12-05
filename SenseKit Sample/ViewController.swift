6322e69c6350dae066559c67c101f2b487db3b03
Paulo Faria <paulo@zewo.io>
2016-12-05 13:18:10 -0200

    Remove dangerous force unwrappings (#3)

--- SenseKit Sample/ViewController.swift
+++ SenseKit Sample/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController : UITableViewController {
         super.viewDidLoad()
         
         UIApplication.shared.isNetworkActivityIndicatorVisible = true
-        let release = NSDictionary(contentsOf: URL(string: videosURL)!)!
-        videos = release.object(forKey: "Videos") as! NSArray
+        
+        if let release = NSDictionary(contentsOf: URL(string: videosURL)!),
+           let videos = release.object(forKey: "Videos") as? NSArray {
+            self.videos = videos
+        }
+        
         UIApplication.shared.isNetworkActivityIndicatorVisible = false
     }
 
@@ -24,28 +28,37 @@ class ViewController : UITableViewController {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
         
-        let video = videos.object(at: indexPath.row) as! NSDictionary
+        guard let video = videos.object(at: indexPath.row) as? NSDictionary else {
+            return cell
+        }
+            
         cell.titleLabel.text = video.object(forKey: "Title") as? String
         cell.desciptionLabel.text = video.object(forKey: "Description") as? String
         cell.durationLabel.text = video.object(forKey: "Duration") as? String
-        cell.thumbnailImageView.setImage(urlString: video.object(forKey: "ImageUrl") as! String)
-        
+        cell.thumbnailImageView.setImage(urlString: video.object(forKey: "ImageUrl") as? String ?? "")
+
         return cell
     }
  
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let video = videos.object(at: indexPath.row) as! NSDictionary
+        guard let video = videos.object(at: indexPath.row) as? NSDictionary else {
+            return
+        }
         
         loadVideoPlayer(
-            url: video.object(forKey: "VideoUrl") as! String,
-            assetId: video.object(forKey: "AssetID") as! String,
-            assetName: video.object(forKey: "Title") as! String,
-            assetType: video.object(forKey: "AssetType") as! String
+            url: video.object(forKey: "VideoUrl") as? String ?? "",
+            assetId: video.object(forKey: "AssetID") as? String ?? "",
+            assetName: video.object(forKey: "Title") as? String ?? "",
+            assetType: video.object(forKey: "AssetType") as? String ?? ""
         )
     }
     
     func loadVideoPlayer(url: String, assetId: String, assetName: String, assetType: String) {
-        let playerItem = AVPlayerItem(url: URL(string: url)!)
+        guard let url = URL(string: url) else {
+            return
+        }
+        
+        let playerItem = AVPlayerItem(url: url)
         
         let player = AVPlayer(playerItem: playerItem)
         let playerViewController = PlayerViewController()
